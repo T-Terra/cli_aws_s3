3eba348d9c0f117d1b07833bc18c724091bfd3e0
T-Terra <gabst138@gmail.com>
2023-08-28 22:21:44 -0300

    feat: includes subcommands

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"cli_aws_s3/aws/s3"
+	"os"
 )
 
 func main() {
-	// s3.Read_files_command()
-	s3.Sub_commands()
+	s3.Read_files_command()
+	subArgs := len(os.Args[2:])
+	if subArgs > 1 {
+		s3.Sub_commands()
+	}
 }
