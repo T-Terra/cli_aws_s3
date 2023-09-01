a3132ae7e4b760d83a8e37b93199e8e4183f9649
Gabriel <gabst138@gmail.com>
2023-08-31 21:59:37 -0300

    feat: add gitignore and command create file

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"cli_aws_s3/aws/s3"
+	"fmt"
+	"os"
 )
 
 func main() {
-	s3.Read_files_command()
+	if len(os.Args) > 1 && os.Args[1] == "-file"{
+		s3.Create_file_command()
+	} else {
+		fmt.Println("Executando outra ação")
+	}
 }
