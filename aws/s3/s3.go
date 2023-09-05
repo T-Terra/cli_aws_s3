1351a23f154f607fe32d5a32ce29428eb9539f28
Gabriel <gabst138@gmail.com>
2023-09-04 23:20:39 -0300

    feat: add func install aws cli

--- aws/s3/s3.go
+++ aws/s3/s3.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 	"flag"
 	"os"
+	"runtime"
+	"os/exec"
 )
+
+type ParamsInstallWin struct {
+	msiexec string
+	param string
+	url_to_download string
+}
 
 var command *string = flag.String("file", "", "Read file with path of files S3")
 
@@ -35,4 +43,59 @@ func Verify_dir() ( txt_result string) {
 		}
 	}
 	return txt_result
+}
+
+
+func ExeInstallAwsCli() {
+	payload := ParamsInstallWin {
+		msiexec: "msiexec.exe",
+		param: "/i",
+		url_to_download: "https://awscli.amazonaws.com/AWSCLIV2.msi",
+	}
+	
+	if runtime.GOOS == "linux" {
+		cmd1 := exec.Command("curl", "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip", "-o", "awscliv2.zip")
+
+		// Comando 2: Descompactar o arquivo awscliv2.zip
+		cmd2 := exec.Command("unzip", "awscliv2.zip")
+
+		// Comando 3: Instalar o AWS CLI
+		cmd3 := exec.Command("sudo", "./aws/install")
+
+		// Execute os comandos em sequência
+		if err := cmd1.Run(); err != nil {
+			fmt.Println("Erro ao baixar o arquivo:", err)
+			os.Exit(1)
+		}
+
+		if err := cmd2.Run(); err != nil {
+			fmt.Println("Erro ao descompactar o arquivo:", err)
+			os.Exit(1)
+		}
+
+		if err := cmd3.Run(); err != nil {
+			fmt.Println("Erro ao instalar o AWS CLI:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("AWS CLI instalado com sucesso!")
+	} else if runtime.GOOS == "windows" {
+		// Comando a ser executado
+		cmd1 := exec.Command(payload.msiexec, payload.param, payload.url_to_download)
+
+		// Configurar a saída padrão e erro para a saída do seu programa
+		cmd1.Stdout = os.Stdout
+		cmd1.Stderr = os.Stderr
+
+		// Executar o comando
+		err := cmd1.Run()
+		if err != nil {
+			fmt.Printf("Erro ao executar o instalação: %v\n", err)
+			return
+		}
+
+		fmt.Println("Comando executado com sucesso!")
+	} else {
+		fmt.Println("Sistema não encontrado")
+	}
 }
